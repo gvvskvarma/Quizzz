e46fa67842bdb2a951ab824836c5082f20d338c5
gvvskvarma <57880602+gvvskvarma@users.noreply.github.com>
2020-01-29 12:13:13 -0600

    Add files via upload

---
+++ Quizzz/quizzz/build/precache-manifest.6ef397d8b276ec56fa2203fde31e1c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42679bf86214c70c5f4f64f2565f5474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a33a51d07c67947c0d",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "2e981089efd4bc8b22bb",
+    "url": "/static/js/2.90b8bb47.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.90b8bb47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6a33a51d07c67947c0d",
+    "url": "/static/js/main.87763ed3.chunk.js"
+  },
+  {
+    "revision": "66121a9fdac6aaf71fef",
+    "url": "/static/js/runtime-main.dbd2bba5.js"
+  }
+]);
